6129d06b525c75d7dfac826fb604f158a88bbc38
Saloni gupta <63607135+Saloni791@users.noreply.github.com>
2022-04-05 18:34:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar />
+      <div className='left'>
+
+        <div className="leftt">
+
+          <div style={{ color: "white", marginBottom: "50px" }} className="media">
+            <h1 style={{ fontSize: "50px", fontWeight: "bold" }}>
+              Proin facilisis risus mus a.
+            </h1>
+            <div style={{ marginTop: "30px" }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+            </div>
+          </div>
+
+          <div className='left2'>
+
+            <div className='search'>
+              <i className="fa fa-search" ></i>
+            </div>
+            <div style={{ color: "white",marginTop:"10px" }}>Lorem Ipsum has been the industry's standard  </div>
+
+          </div>
+
+          <div className='left2'>
+
+            <div className='lock'>
+              <i className="fa fa-lock"></i>
+            </div>
+            <div style={{ color: "white",marginTop:"10px"  }}>Lorem Ipsum has been the industry's standard </div>
+          </div>
+        </div>
+
+      </div>
+
+
+      <div className='right'>
+
+        <div className='rightt' >
+
+          <div className='right1'>
+
+            <div className='delete'>
+              <i className="fa fa-trash"></i>
+            </div>
+
+            <div>
+              <div >
+                Feature
+              </div>
+              <div >
+                <p>
+                  Lorem Ipsum has been the industry's standard dummy
+                </p>
+                <p>                
+                  Lorem Ipsum has been the industry's standard dummy
+                </p>
+              </div>
+             
+            </div>
+
+          </div>
+
+          <div className='right1'>
+
+            <div className='delete'>
+              <i className="fa fa-trash"></i>
+            </div>
+
+            <div>
+              <div >
+                Feature
+              </div>
+              <div >
+                <p>
+                  Lorem Ipsum has been the industry's standard dummy
+                </p>
+                <p>                
+                  Lorem Ipsum has been the industry's standard dummy
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className='right1'>
+
+            <div className='delete'>
+              <i className="fa fa-trash"></i>
+            </div>
+
+            <div>
+              <div >
+                Feature
+              </div>
+              <div >
+                <p>
+                  Lorem Ipsum has been the industry's standard dummy
+                </p>
+                <p>                
+                  Lorem Ipsum has been the industry's standard dummy
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className='navbar' >
+            <div className='nav1'>
+                <b className='logo'>
+                    Logo
+                </b>
+                <div className="toggle">
+                    <i className='fa fa-bars '></i>
+                </div>
+            </div>
+        </div>
+    )
+}
